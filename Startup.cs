7410f1112d10d3a08a02ced95047bd043b440a9a
hankjf <hankjf@yonyou.com>
2020-06-16 11:47:12 +0800

    更改记住密码时间为2天

--- Startup.cs
+++ Startup.cs
@@ -68,7 +68,7 @@ namespace RazorSecond
                 {
                     // Cookie settings
                     options.Cookie.HttpOnly = true;
-                    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                    options.ExpireTimeSpan = TimeSpan.FromDays(2);
 
                     options.LoginPath = "/Identity/Account/Login";
                     options.AccessDeniedPath = "/Identity/Account/AccessDenied";
