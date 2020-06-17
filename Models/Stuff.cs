4a3bd3eb0fe1fe8710e1f50e2c2233369f44f8db
hankjf <hankjf@yonyou.com>
2020-06-17 10:40:02 +0800

    必输字段红星设置

--- Models/Stuff.cs
+++ Models/Stuff.cs
@@ -9,7 +9,7 @@ namespace RazorSecond.Models
 
         [MaxLength(10)]
         [Display(Name = "姓名")]
-        [Required]
+        [Required(ErrorMessage = "123")]
         public string Name { get; set; }
 
         [Display(Name = "性别")]
@@ -25,16 +25,17 @@ namespace RazorSecond.Models
 
     public enum Gender
     {
+
+        [Display(Name = "男")]
+        male = 1,
         [Display(Name = "女")]
         female,
-        [Display(Name = "男")]
-        male,
     }
 
     public enum Department
     {
         [Display(Name = "眼科")]
-        眼科,
+        眼科 = 1,
         [Display(Name = "口腔科")]
         口腔科,
         [Display(Name = "骨科")]
@@ -44,7 +45,7 @@ namespace RazorSecond.Models
     public enum Title
     {
         [Display(Name = "主任医师")]
-        主任医师,
+        主任医师 = 1,
         [Display(Name = "副主任医师")]
         副主任医师,
 
