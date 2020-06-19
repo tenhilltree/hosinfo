d142c35656ab023132e805b768dcbcccdb8e1757
hankjf <hankjf@yonyou.com>
2020-06-19 11:57:07 +0800

    medicine数据seed

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -24,6 +24,19 @@ namespace RazorSecond.Models
 
             public string Department { get; set; }
         }
+
+        public class Medicine1
+        {
+            public string Code { get; set; }
+            public string Name { get; set; }
+
+            public int Exceed { get; set; }
+            public string Type { get; set; }
+
+            public string ProDate { get; set; }
+
+            public string Factory { get; set; }
+        }
         public static void Initialize(IServiceProvider serviceProvider)
         {
 
@@ -47,6 +60,24 @@ namespace RazorSecond.Models
                                 Gender = (Gender)Enum.Parse(typeof(Gender), item.Gender),
                                 Title = (Title)Enum.Parse<Title>(item.Title),
                                 Department = (Department)Enum.Parse<Department>(item.Department),
+                            });
+                        }
+                    }
+                    using (StreamReader r = new StreamReader("Models/medicine.json"))
+                    {
+                        string json = r.ReadToEnd();
+                        IList<Medicine1> array = JsonConvert.DeserializeObject<IList<Medicine1>>(json);
+
+                        foreach (var item in array)
+                        {
+                            context.Medicine.Add(new Medicine
+                            {
+                                Code = item.Code,
+                                Name = item.Name,
+                                Type = (MedicineType)Enum.Parse(typeof(MedicineType), item.Type),
+                                ReleaseDate = DateTime.Parse(item.ProDate),
+                                LeftMonths = item.Exceed,
+                                Factory = item.Factory,
                             });
                         }
                     }
